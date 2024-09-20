ebeefbeb61e8287a5e54f0cd0a0f4abf687763e0
Timoniann <timoniann@gmail.com>
2024-09-20 12:16:39 +0300

    fix: class alias export fix

--- src/aliases.ts
+++ src/aliases.ts
@@ -1,13 +1,16 @@
-import {MutableVariable} from "./vars"
 import {Variable} from "./variable"
-import {ConstantVariable} from "./vars";
+import {MutableVariable} from "./vars"
+import {ConstantVariable} from "./vars"
+import {FuncVariable} from "./vars"
 
-export type Var<T> = Variable<T>
-
-export type MutableVar<T> = MutableVariable<T>
-export type Vary<T> = MutableVariable<T>
-
-export type ConstVariable<T> = ConstantVariable<T>
-export type ConstVar<T> = ConstantVariable<T>
-export type ImmutableVar<T> = ConstantVariable<T>
-export type ReadonlyVar<T> = ConstantVariable<T>
+export {
+  Variable as Var,
+  MutableVariable as MutableVar,
+  MutableVariable as Vary,
+  ConstantVariable as ConstVariable,
+  ConstantVariable as ConstVar,
+  ConstantVariable as ImmutableVar,
+  ConstantVariable as ReadonlyVar,
+  FuncVariable as FuncVar,
+  FuncVariable as LazyVariable,
+}
